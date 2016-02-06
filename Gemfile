8e63317b5067055fdb06286be0bb965bfed8c28e
san3091 <san3091@gmail.com>
2016-02-06 13:44:13 -0500

    scaffolding addition: descriptions to item and projects

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ group :development, :test do
 end
 
 group :development do
+  # generates erd diagram in pdf
+  gem 'rails-erd'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206174118) do
+ActiveRecord::Schema.define(version: 20160206184220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160206174118) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "decription"
   end
 
   create_table "phases", force: :cascade do |t|
@@ -29,8 +30,9 @@ ActiveRecord::Schema.define(version: 20160206174118) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "sources", force: :cascade do |t|
